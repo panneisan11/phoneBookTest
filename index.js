ca4b7ce1588c76ab4f3623e4ef4f2b80a1d08823
panneisan <panneisan11@gmail.com>
2024-06-28 19:09:17 +0630

    'add_path'

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 const cors = require("cors");
+const path = require('path');
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'dist')));
